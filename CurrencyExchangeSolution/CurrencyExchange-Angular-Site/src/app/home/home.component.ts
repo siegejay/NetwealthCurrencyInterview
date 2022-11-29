c223f942c54bd7b53a54950a05fe78bea3598b8a
Chris Johnson <chris@siegejay.co.uk>
2022-11-29 14:19:27 +0000

    GitHub Dependabot security alert fixes

--- CurrencyExchangeSolution/CurrencyExchange-Angular-Site/src/app/home/home.component.ts
+++ CurrencyExchangeSolution/CurrencyExchange-Angular-Site/src/app/home/home.component.ts
@@ -15,14 +15,19 @@ import { ExchangeCard } from './exchange-card';
 })
 export class HomeComponent implements OnInit {
 
+  private readonly CurrencyRegExValidator = Validators.pattern(/^[-]?[0-9]+(\.[0-9]{1,2})?$/);
+
   // The view title
-  title?: string;
+  title?: string = 'Code Demo Currency Exchange Calculator';
 
   // the form model
   form!: FormGroup;
 
+  // Form Submitted state
+  formIsSubmitted: boolean = false;
+
   // the exchange result
-  exchangeResult!: ExchangeCard; 
+  exchangeResult!: ExchangeCard;
 
   // the currencies observable for the select (using async pipe)
   currencies?: Observable<Currency[]>;
@@ -33,7 +38,7 @@ export class HomeComponent implements OnInit {
     this.form = new FormGroup({
       from: new FormControl('', Validators.required),
       to: new FormControl('', Validators.required),
-      value: new FormControl('', Validators.required)
+      value: new FormControl('', [Validators.required, this.CurrencyRegExValidator])
     });
 
     this.loadData();
@@ -51,6 +56,8 @@ export class HomeComponent implements OnInit {
   }
 
   exchangeValue() {
+    this.formIsSubmitted = true;
+
     var from = this.form.get('from')?.value;
     var value = this.form.get('value')?.value;
     var to = this.form.get('to')?.value;
@@ -58,4 +65,32 @@ export class HomeComponent implements OnInit {
       .exchange(from, value, to)
       .subscribe(result => this.exchangeResult = result, error => console.error(error));
   }
+
+  getErrors(
+    control: AbstractControl,
+    displayName: string,
+    customMessages: { [key: string]: string } | null = null
+  ): string[] {
+    var errors: string[] = [];
+    Object.keys(control.errors || {}).forEach((key) => {
+      switch (key) {
+        case 'required':
+          errors.push(`${displayName} ${customMessages?.[key] ?? "is required."}`);
+          break;
+        case 'pattern':
+          errors.push(`${displayName} ${customMessages?.[key] ?? "contains invalid characters."}`)
+          break;
+        default:
+          errors.push(`${displayName} is invalid.`)
+          break;
+      }
+    });
+    return errors;
+  }
+
+  get formCard() {
+    return this.form.controls;
+  }
+
+  
 }
